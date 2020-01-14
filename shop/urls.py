300d1f1486b85e180aa3290090ae946451c1be43
andrei <andrei@example.com>
2020-01-14 19:22:28 +0200

    images

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import ShoesPageView
 
 urlpatterns = [
-    path('', views.homepage, name='home'),
+    # path('', views.homepage, name='home'),
     path('product', views.product, name='product'),
+    path('', ShoesPageView.as_view(), name='home'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
 def homepage(request):
     return render(request, 'productlist.html', {})
 
 def product(request):
     return render(request, 'product.html', {})
+
+class ShoesPageView(ListView):
+    model = Post
+    template_name = 'productlist.html'
